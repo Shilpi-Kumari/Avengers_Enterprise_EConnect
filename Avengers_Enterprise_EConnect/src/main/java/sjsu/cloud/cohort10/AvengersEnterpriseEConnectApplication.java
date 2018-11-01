b3b9f96d4e5187bc3d129a91a6b0650d14d371e8
sumanthravipatirr <sumanthravipatirr@gmail.com>
2018-10-31 18:24:32 -0700

    Project Creation

---
+++ Avengers_Enterprise_EConnect/src/main/java/sjsu/cloud/cohort10/AvengersEnterpriseEConnectApplication.java
@@ -0,0 +1,12 @@
+package sjsu.cloud.cohort10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersEnterpriseEConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersEnterpriseEConnectApplication.class, args);
+	}
+}
